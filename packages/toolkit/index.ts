d1819c82ff3668d69f8fba405ea583e5ad81d7c1
The Ult <ult_dev@pm.me>
2026-01-28 01:15:25 +0100

    chore: add comments for ng-packagr and eslint configuration in index.ts

--- packages/toolkit/index.ts
+++ packages/toolkit/index.ts
@@ -3,4 +3,6 @@
 // - @ngx-signal-forms/toolkit (primary)
 // - @ngx-signal-forms/toolkit/core (secondary, optional)
 
+// ng-packagr allows relative imports between entry points within the same package
+// eslint-disable-next-line @nx/enforce-module-boundaries
 export * from './core';
